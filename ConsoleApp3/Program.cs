86484f89e91097326f432a02cfc75a645c8f4850
visualstudio <visualstudio@230-PC11>
2019-03-07 16:32:05 -0500

    lab 5

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,40 +8,33 @@ namespace C0714696_assignment2
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
             //Student Name Manpreet Singh Student ID C0714696
             //CSD3354 Section 2
             //Assignment 2
             //March 6, 2019 
             //TO DO: Call Method2 from the DelegateExercises Class
-            DelegateExercises a = new DelegateExercises();
-            a.Method2();
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
         }
     }
 }
 
 public class DelegateExercises
 {
-    public delegate int MyDelegate(int intValue);
-    public int Method1(int intMethod1)
+    public delegate int MyDelegate();
+   
+    void Method1()
     {
-        return intMethod1 * 2;
-    }
-
-    public int Method2(int intMethod2)
-    {
-        return intMethod2 * 10;
+        System.Console.WriteLine("MyDelegate");
     }
 
     public void Method3()
     {
         MyDelegate myDelegate = new MyDelegate(Method1);
-        int result1 = myDelegate(10);
-        System.Console.WriteLine(result1);
-        myDelegate = new MyDelegate(Method2);
-        int result2 = myDelegate(10);
-        Sytem.Console.Writeline(result2);
+        myDelegate();
+
 
     }
 }
